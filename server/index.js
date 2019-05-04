8a01ce005ec59b12f4fd344f8a2ae82073985ae2
Anna Pojawis <annapojawis@gmail.com>
2019-05-03 22:28:13 -0700

    finalize code

--- server/index.js
+++ server/index.js
@@ -5,42 +5,42 @@ const app = express();
 const port = process.env.PORT || 3000;
 const axios = require('axios');
 
-// const axios3001 = axios.create({
-//   baseURL: 'http://54.161.138.158',
-// });
-const axios3002 = axios.create({
-  baseURL: 'http://ec2-52-53-216-52.us-west-1.compute.amazonaws.com',
+const axios3001 = axios.create({
+  baseURL: 'http://ec2-54-67-119-225.us-west-1.compute.amazonaws.com',
 });
-// const axios3003 = axios.create({
-//   baseURL: 'http://35.165.224.178',
-// });
-// const axios3004 = axios.create({
-//   baseURL: 'http://34.219.173.69',
-// });
+const axios3002 = axios.create({
+  baseURL: 'http://ec2-54-153-51-4.us-west-1.compute.amazonaws.com',
+});
+const axios3003 = axios.create({
+  baseURL: 'http://ec2-54-67-75-182.us-west-1.compute.amazonaws.com',
+});
+const axios3004 = axios.create({
+  baseURL: 'http://ec2-3-214-157-167.compute-1.amazonaws.com',
+});
 
-// app.get('/api/restaurants/:id/menus', (req, res) => {
-//   axios3004.get(`/api/restaurants/${req.params.id}/menus`)
-//     .then(response => res.send(response.data))
-//     .catch(error => res.send(error));
-// });
+app.get('/api/restaurants/:id/menus', (req, res) => {
+  axios3004.get(`/api/restaurants/${req.params.id}/menus`)
+    .then(response => res.send(response.data))
+    .catch(error => res.send(error));
+});
 
-// app.get('/api/restaurants/:id/menus/:menu', (req, res) => {
-//   axios3004.get(`/api/restaurants/${req.params.id}/menus/${req.params.menu}`)
-//     .then(response => res.send(response.data))
-//     .catch(error => res.send(error));
-// });
+app.get('/api/restaurants/:id/menus/:menu', (req, res) => {
+  axios3004.get(`/api/restaurants/${req.params.id}/menus/${req.params.menu}`)
+    .then(response => res.send(response.data))
+    .catch(error => res.send(error));
+});
 
-// app.get('/api/restaurants/:id/info', (req, res) => {
-//   axios3003.get(`/api/restaurants/${req.params.id}/info`)
-//     .then(response => res.send(response.data))
-//     .catch(error => res.send(error));
-// });
+app.get('/api/restaurants/:id/info', (req, res) => {
+  axios3003.get(`/api/restaurants/${req.params.id}/info`)
+    .then(response => res.send(response.data))
+    .catch(error => res.send(error));
+});
 
-// app.get('/api/restaurants/:id/overview', (req, res) => {
-//   axios3003.get(`/api/restaurants/${req.params.id}/overview`)
-//     .then(response => res.send(response.data))
-//     .catch(error => res.send(error));
-// });
+app.get('/api/restaurants/:id/overview', (req, res) => {
+  axios3003.get(`/api/restaurants/${req.params.id}/overview`)
+    .then(response => res.send(response.data))
+    .catch(error => res.send(error));
+});
 
 app.get('/api/restaurants/:id/photos', (req, res) => {
   axios3002.get(`/api/restaurants/${req.params.id}/photos`)
@@ -54,11 +54,11 @@ app.post('/api/restaurant/:id/photos', (req, res) => {
     .catch(error => res.send(error));
 });
 
-// app.get('/api/restaurants/:id/reviews', (req, res) => {
-//   axios3001.get(`/api/restaurants/${req.params.id}/reviews`)
-//     .then(response => res.send(response.data))
-//     .catch(error => res.send(error));
-// });
+app.get('/api/restaurants/:id/reviews', (req, res) => {
+  axios3001.get(`/api/restaurants/${req.params.id}/reviews`)
+    .then(response => res.send(response.data))
+    .catch(error => res.send(error));
+});
 
 
 // app.use(morgan('dev'));
